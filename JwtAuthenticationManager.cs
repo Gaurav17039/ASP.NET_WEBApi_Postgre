7bf13f7d0a5507c47d966b78d9b6a86fea1ca59e
Gaurav17039 <83353437+Gaurav17039@users.noreply.github.com>
2023-07-01 03:42:36 +0530

    Add files via upload

---
+++ JwtAuthenticationManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+
+namespace CRUD_Migration_Logging_XunitTesting
+{
+    public class JwtAuthenticationManager
+    {
+        private readonly string key;
+
+        private readonly IDictionary<string, string> users = new Dictionary<string, string>()
+        {{"test", "password"}, {"test1", "pwd" } };
+
+        public JwtAuthenticationManager(string key)
+        {
+            this.key = key;
+        }
+
+        public string Authenticate(string username, string password)
+        {
+            if(!users.Any(u => u.Key == username && u.Value == password))
+            { return null; }
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            var tokenkey = Encoding.ASCII.GetBytes(key);
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenkey),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
